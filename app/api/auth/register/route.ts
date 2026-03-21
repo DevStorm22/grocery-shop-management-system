e8c3db35d738da8065acee1b3734e598e3122f49
Kaushik <kaushikboss0@gmail.com>
2026-03-21 11:34:40 +0530

    feat: implement user model and register API with password hashing

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "../../../src/lib/db";
+import { User } from "../../../src/models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+    try {
+        const { name, email, password } = await req.json();
+        await connectDB();
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return NextResponse.json({
+                status: 400,
+                message: "User already exists"
+            })
+        }
+        const  hashedPassword = await bcrypt.hash(password, 10);
+        const user = User.create({
+            name, email, password: hashedPassword
+        });
+        return NextResponse.json({
+            status: 201,
+            message: "User Created",
+        });
+    }
+    catch (error) {
+        return NextResponse.json({
+            status: 500,
+            message: "Internal Server Problem",
+        });
+    }
+}
---
+++ app/src/models/User.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: (value: string) => {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            },
+            message: "Invalid email address"
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 100,
+        validate: {
+            validator: (value: string) => {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+            },
+            message: "Invalid password"
+        }
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'staff', 'customer'],
+        default: 'customer',
+    },
+},{ timestamps: true }
+);
+
+// Reuse the existing model if it was already registered (prevents overwrite errors in hot reload).
+export const User = (mongoose.models.User as mongoose.Model<any>) || mongoose.model("User", userSchema);
